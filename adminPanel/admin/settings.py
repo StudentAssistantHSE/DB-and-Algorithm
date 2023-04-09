412d429ba067ae64863d2e505109f861d2cdb839
Crazylamb <71339600+Crazylamb@users.noreply.github.com>
2023-04-09 22:02:38 +0300

    Menu fix

--- adminPanel/admin/settings.py
+++ adminPanel/admin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b^06t^u$1v2t3$etc)dsm_=*z=*9-07(oh3-zi6$nbv#kv*39$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost:1235", "127.0.0.1:1235", "127.0.0.1", "localhost"]
 
