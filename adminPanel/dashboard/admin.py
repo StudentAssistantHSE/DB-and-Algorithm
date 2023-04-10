f21610cacee09d4e900d62ad95524cc5daec0c28
Crazylamb <71339600+Crazylamb@users.noreply.github.com>
2023-04-10 06:11:33 +0300

    Menu fix

--- adminPanel/dashboard/admin.py
+++ adminPanel/dashboard/admin.py
@@ -6,6 +6,7 @@ from django.contrib.auth.admin import UserAdmin
 
 class StatusesAdmin(admin.ModelAdmin):
     list_display = ["status"]
+    search_fields = ('status',)
 
     def getStatus(self, obj):
         return obj.status.name
@@ -15,6 +16,7 @@ class StatusesAdmin(admin.ModelAdmin):
 
 class FacultyAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "shortname"]
+    search_fields = ('name', 'shortname')
 
     def getFaculty(self, obj):
         return obj.faculty.name
@@ -24,6 +26,8 @@ class FacultyAdmin(admin.ModelAdmin):
 
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ["project", "applicant", "created_date", "message", "status"]
+    search_fields = ("project__name", "applicant__fullname", "status__status")
+    list_filter = ["status"]
 
     def getAppliсation(self, obj):
         return obj.project.name
@@ -33,7 +37,8 @@ class ApplicationAdmin(admin.ModelAdmin):
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["category", "is_custom"]
-
+    search_fields = ("category", )
+    list_filter = ["is_custom"]
     def getCategory(self, obj):
         return obj.category.name
 
@@ -42,6 +47,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class ProjectsAdmin(admin.ModelAdmin):
     list_display = ["name", "description"]
+    search_fields = ("name", )
 
     def getProjects(self, obj):
         return obj.project.name
@@ -50,18 +56,25 @@ class ProjectsAdmin(admin.ModelAdmin):
 
 class ProjectCategoryAdmin(admin.ModelAdmin):
     list_display = ["project", "category"]
+    search_fields = ("project__name", "category__category")
 
 class ProjectTimetableAdmin(admin.ModelAdmin):
     list_display = ["project", "deadline", "name", "description"]
+    search_fields = ("project__name", "name")
 
 class UserCategoryAdmin(admin.ModelAdmin):
     list_display = ["user", "category"]
+    search_fields = ("user__fullname", "category__category")
 
 class UserRecommendationsAdmin(admin.ModelAdmin):
     list_display = ["user", "project", "generated_date"]
+    search_fields = ("user__fullname", "project__name")
+    list_filter = ["generated_date"]
 
 class UsersAdmin(admin.ModelAdmin):
     list_display = ["email", "fullname", "last_login", "description", "bio", "faculty", "password", "created_date", "updated_date"]
+    search_fields = ("email", "fullname", "faculty__name")
+    list_filter = ["faculty", "created_date"]
 
 
 
